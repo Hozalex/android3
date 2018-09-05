267be9ca0cc16f710e09fadf1ced7135a050cd82
Hozalex <hozalex@gmail.com>
2018-09-05 10:45:05 +0300

    add app bar

--- app/src/main/java/com/ahozyainov/cloudshare/view/activity/MainActivity.kt
+++ app/src/main/java/com/ahozyainov/cloudshare/view/activity/MainActivity.kt
@@ -2,14 +2,39 @@ package com.ahozyainov.cloudshare.view.activity
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.widget.Toast
 import com.ahozyainov.cloudshare.R
+import kotlinx.android.synthetic.main.app_bar.*
 
-class MainActivity : AppCompatActivity()
+class MainActivity : AppCompatActivity(), View.OnClickListener
 {
 
     override fun onCreate(savedInstanceState: Bundle?)
     {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean
+    {
+        menuInflater.inflate(R.menu.app_bar_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean
+    {
+        Toast.makeText(this, "search menu", Toast.LENGTH_SHORT).show()
+        return super.onOptionsItemSelected(item)
+    }
+
+    override fun onClick(p0: View?)
+    {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+
 }
