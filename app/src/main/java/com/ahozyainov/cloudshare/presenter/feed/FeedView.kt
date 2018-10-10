5902b31933e1c989c2729104f4ffed6103d8fc1d
hozalex <hozalex@gmail.com>
2018-10-10 16:18:57 +0300

    change presenter view interface

--- app/src/main/java/com/ahozyainov/cloudshare/presenter/feed/FeedView.kt
+++
@@ -1,7 +0,0 @@
-package com.ahozyainov.cloudshare.presenter.feed
-
-import com.ahozyainov.cloudshare.presenter.base.BaseRestView
-
-interface FeedView : BaseRestView {
-    fun setItem(items: List<String>)
-}
--- app/src/main/java/com/ahozyainov/cloudshare/presenter/search/SearchView.kt
+++
@@ -1,7 +0,0 @@
-package com.ahozyainov.cloudshare.presenter.search
-
-import com.ahozyainov.cloudshare.presenter.base.BaseRestView
-
-interface SearchView : BaseRestView {
-    fun setItem(items: List<String>)
-}
