eda503b66b0b85293d2a8096f4aff69b4fb2465b
hozalex <hozalex@gmail.com>
2018-10-11 14:14:06 +0300

    add string resourses

--- app/src/main/java/com/ahozyainov/cloudshare/presenter/profile/ProfileView.kt
+++
@@ -1,8 +0,0 @@
-package com.ahozyainov.cloudshare.presenter.profile
-
-import com.ahozyainov.cloudshare.presenter.base.BasePresenterView
-
-interface ProfileView : BasePresenterView {
-    fun setData(userName: String, url: String)
-    override fun showData(urlItems: List<String>, descriptionItems: List<String>) {}
-}
